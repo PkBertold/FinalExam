fe37e9b92283fa342c444a5aea8c0fcb8b3644f1
Crosshair <bertold95pk@gmail.com>
2022-01-11 19:43:02 +0100

    v 1.0

---
+++ Javascript/objects.js
@@ -0,0 +1,26 @@
+/*  ↓  Alaplap  ↓  */
+const motherboard = {
+    Name = "",
+    RamSlot = "",
+    Brand = "",
+    Slots = 0
+    CPUSlot = ""
+};
+
+
+
+/*  ↑  Alaplap  ↑  */
+
+
+/*  ↓  RAM  ↓  */
+const ram = {
+    Name = "",
+    RamSlot = "",
+    //Brand = "",
+    //Slots = 0
+    //CPUSlot = ""
+};
+
+
+
+/*  ↑  RAM  ↑  */

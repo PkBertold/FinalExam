834761365a02483a757f37018c7c332da7f3d644
Crosshair <bertold95pk@gmail.com>
2022-01-11 22:01:32 +0100

    v 1.0.2

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -1,26 +1,89 @@
 /*  ↓  Alaplap  ↓  */
 const motherboard = {
     Name = "",
-    RamSlot = "",
-    Brand = "",
-    Slots = 0
-    CPUSlot = ""
+    //Capacity = ,
+    RamSlot = "",   //[DDR3 , DDR4]
+    Brand = "",     //[Intel , AMD]
+    Slots = 0       //
+    CPUSlot = ""    //[AM4 , LGA1151 , LGA1200]
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
 };
-
-
-
 /*  ↑  Alaplap  ↑  */
+
+
+/*  ↓  Processor  ↓  */
+const cpu = {
+    Name = "",      
+    Capacity = ,    //Ide írhatjuk a processzor órajelét
+    //RamSlot = "", //[DDR3 , DDR4]
+    Brand = "",     //[Intel , AMD]
+    //Slots = 0     
+    CPUSlot = ""    //[AM4 , LGA1151 , LGA1200]
+    //StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+};
+/*  ↑  Processor  ↑  */
 
 
 /*  ↓  RAM  ↓  */
 const ram = {
     Name = "",
-    RamSlot = "",
+    Capacity = ,    //[4Gb , 8Gb]
+    RamSlot = "",   //[DDR3 , DDR4]
+    //Brand = "",   //--Ezt később mert nem fontos--
+    //Slots = 0       
+    //CPUSlot = ""  //[AM4 , LGA1151 , LGA1200]
+    //StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+};
+/*  ↑  RAM  ↑  */
+
+
+/*  ↓  Storage  ↓  */
+const storage = {
+    Name = "",
+    Capacity = ,       //[500Gb , 1Tb , 2Tb]
+    //RamSlot = "",
+    //Brand = "",      //--opcionális--
+    //Slots = 0
+    //CPUSlot = ""
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+};
+/*  ↑  Storage  ↑  */
+
+
+/*  ↓  StorageHDD  ↓  */
+const hdd = {
+    Name = "",
+    Capacity = ,        //[500Gb , 1Tb , 2Tb]
+    //RamSlot = "",
+    //Brand = "",       //--opcionális--
+    //Slots = 0
+    //CPUSlot = ""
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+};
+/*  ↑  StorageHDD  ↑  */
+
+
+/*  ↓  StorageSSD  ↓  */
+const hdd = {
+    Name = "",          
+    Capacity = ,        //[500Gb , 1Tb , 2Tb]
+    //RamSlot = "",
+    //Brand = "",       //--opcionális--
+    //Slots = 0
+    //CPUSlot = ""
+    StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
+};
+/*  ↑  StorageSSD  ↑  */
+
+
+/*  ↓  Power Supply  ↓  */
+const pwrsply = {
+    Name = "",
+    Capacity = ,        //[500W , 600W , 700W]
+    //RamSlot = "",
     //Brand = "",
     //Slots = 0
     //CPUSlot = ""
+    //StorageSlot =   //[1-8 / Itt a szám után nem kell vessző azt hiszem]
 };
-
-
-
-/*  ↑  RAM  ↑  */
+/*  ↑  Power Supply  ↑  */
